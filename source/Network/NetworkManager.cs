c9b48862303a0a344b5020a0064642366cf408cf
Giovanni Voltan <g1opto@proton.me>
2024-11-29 20:30:54 +0100

    Add Network Manager

---
+++ source/Network/NetworkManager.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * NetworkManager in saor.Network
+ * 
+ * The saor project
+ * © 2024 Giovanni Voltan
+ * Code released under the BSD 3-Clause "New" or "Revised" License
+ * License information: https://github.com/g1opto/saor/blob/main/LICENSE
+ * Project link: https://github.com/g1opto/saor
+ */
+
+using Cosmos.System.Network.IPv4.UDP.DHCP;
+
+namespace saor.Network
+{
+    public class NetworkManager
+    {
+        public NetworkManager()
+        {
+            _ = new DHCPClient().SendDiscoverPacket();
+
+        }
+    }
+}
